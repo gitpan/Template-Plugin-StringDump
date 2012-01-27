958a8bb3cd3d43e9f53a2aa22450a7564c84ce57
Nick Patch <patch@cpan.org>
2012-01-27 03:30:08 +0000

    Import of PATCH/Template-Plugin-StringDump-0.04 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-StringDump
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         PATCH/Template-Plugin-StringDump-0.04.tar.gz
    gitpan-cpan-author:       PATCH
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/StringDump.pm
+++ lib/Template/Plugin/StringDump.pm
@@ -3,28 +3,20 @@ package Template::Plugin::StringDump;
 use 5.006;
 use strict;
 use warnings;
+use utf8;
 use parent qw( Template::Plugin::Filter );
-use String::Dump;
+use String::Dump qw( dump_hex dump_dec dump_oct dump_bin dump_names );
 
-our $VERSION = '0.03';
-our $DYNAMIC = 1;  # dynamic filter
-
-use constant FILTER_NAME => 'dump';
+our $VERSION = '0.04';
 
 sub init {
     my ($self) = @_;
 
-    $self->install_filter(FILTER_NAME);
+    for my $filter (qw< dump_hex dump_dec dump_oct dump_bin dump_names >) {
+        $self->{_CONTEXT}->define_filter($filter => \&$filter);
+    }
 
     return $self;
-}
-
-sub filter {
-    my ($self, $string, $args, $conf) = @_;
-    my ($mode) = @{$args};
-
-    return dumpstr($string) unless $mode;
-    return dumpstr($mode, $string);
 }
 
 1;
@@ -35,11 +27,11 @@ __END__
 
 =head1 NAME
 
-Template::Plugin::StringDump - String::Dump plugin for TT
+Template::Plugin::StringDump - String::Dump filter for TT
 
 =head1 VERSION
 
-This document describes Template::Plugin::StringDump version 0.03.
+This document describes Template::Plugin::StringDump version 0.04.
 
 =head1 SYNOPSIS
 
@@ -49,37 +41,26 @@ Template:
 
     [% SET msg = 'Ĝis! ☺' %]
 
-    hex: [% msg | dump %]
-    dec: [% msg | dump('dec') %]
-    oct: [% msg | dump('oct') %]
-    bin: [% msg | dump('bin') %]
-    names: [% msg | dump('names') %]
+    hex: [% msg | dump_hex %]
+    dec: [% msg | dump_dec %]
+    oct: [% msg | dump_oct %]
+    bin: [% msg | dump_bin %]
 
 Output:
 
-    hex: 11C 69 73 21 20 263A
-    dec: 284 105 115 33 32 9786
-    oct: 434 151 163 41 40 23072
-    bin: 100011100 1101001 1110011 100001 100000 10011000111010
-    names: LATIN CAPITAL LETTER G WITH CIRCUMFLEX, LATIN SMALL LETTER I,
-    LATIN SMALL LETTER S, EXCLAMATION MARK, SPACE, WHITE SMILING FACE
-
-The output has been manually split into multiple lines for the layout of this
-document.
+    hex: C4 9C 69 73 21 20 E2 98 BA
+    dec: 196 156 105 115 33 32 226 152 186
+    oct: 304 234 151 163 41 40 342 230 272
+    bin: 11000100 10011100 1101001 1110011 100001 100000 11100010 10011000 10111010
 
 =head1 DESCRIPTION
 
-This L<Template::Toolkit> plugin adds the C<dump> filter, which dumps strings
-of characters or bytes for display and debugging.  This filter is a simple
-wrapper around the C<dumpstr> function from the L<String::Dump> module.  The
-filter takes the C<dumpstr> mode as an optional argument, defaulting to
-C<hex>.  See L<String::Dump> for details.
-
-The filter name C<dump> was selected instead of C<dumpstr> because filters
-only work on strings, so the name would be repetitive.  It also doesn't
-compete with other TT plugins like L<Template::Plugin::Dump> or
-L<Template::Plugin::Dumper> because they need to function on data structures,
-as opposed to strings, so they don't provide filters.
+This L<Template::Toolkit> plugin adds five filters for dumping strings for
+display and debugging: C<dump_hex>, C<dump_dec>, C<dump_oct>, C<dump_bin>,
+and C<dump_names>.  Each octet (byte) is dumped on encoded strings and each
+character’s code point on decoded strings.  These filters are simple wrappers
+around the functions of the same names from L<String::Dump>.  See that module
+for details.
 
 =head1 AUTHOR
 
@@ -87,7 +68,7 @@ Nick Patch <patch@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2011 Nick Patch
+© 2011–2012 Nick Patch
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
